ede4aacfe2089a08fe67b5fb2518b9b3968ebfe7
Von <Adios_verrickt@hotmail.com>
2016-11-15 16:19:08 +0800

    Add SQL scripts

---
+++ SQL/TSP_Init.sql
@@ -0,0 +1,131 @@
+create database TSP;
+use TSP;
+create table User
+(
+	Name nvarchar(10) not null,
+	ID int not null AUTO_INCREMENT primary key,
+    Email nvarchar(50) not null,
+    Gender char(1) not null check(Gender in ('M','F')),
+	Age int not null check(Age>=0 and Age<=120),
+    PhoneNumber nvarchar(11),
+    Password nvarchar(20) not null
+);
+
+create table MemberShipCardLevel
+(
+	LevelID int not null auto_increment primary key,
+    LevelName nvarchar(20) not null
+);
+
+create table MemberShipCard
+(
+ID int not null auto_increment primary key,
+Fund decimal(10,2) not null check(Fund>=0),
+MemberShipLevel int not null,
+FOREIGN KEY(MemberShipLevel) references User(ID)
+);
+create table HealthyDocument
+(
+ID int not null auto_increment primary key,
+UserID int not null,
+Notes nvarchar(200) not null,
+foreign key(UserID) references User(ID)
+
+);
+create table Employee
+(
+	ID int not null AUTO_INCREMENT primary key,
+    Email nvarchar(50) not null,
+    Gender char(1) not null check(Gender in ('M','F')),
+	Age int not null check(Age>=0 and Age<=120),
+    PhoneNumber nvarchar(11),
+    JobTitle nvarchar(10) not null,
+    Name nvarchar(10) not null,
+    Password nvarchar(20) not null,
+    Authority int not null check(Authority in (0,1,2))
+);
+
+create table Service
+(
+	ID int not null auto_increment primary key,
+    Name nvarchar(20) not null
+);
+
+create table Technician
+(
+	ID int not null auto_increment primary key,
+    EmployeeID int not null,
+    foreign key(EmployeeID) references Employee(ID)
+);
+
+create table DedicatedService
+(
+ ID int not null auto_increment primary key,
+ TechnicianID int not null,
+ ServiceID int not null,
+ Price decimal(10,2) not null check(Price>0),
+ Foreign key (ServiceID) references Service(ID),
+ Foreign key (TechnicianID) references Technician(ID)
+
+);
+create table DaylyAvailibility
+(
+ID int not null auto_increment primary key,
+StartTime time not null check(StartTime between '00:00:00' and '24:00:00'),
+EndTime time  not null,
+DayOfWeek int  not null check(DayOfWeek between 1 and 7),
+TechnicianID int  not null,
+foreign key (TechnicianID) references Technician(ID)
+);
+
+create table PreorderTicket
+(
+ID int  not null auto_increment primary key,
+OrderedTime DateTime not null,
+Status nvarchar(10) check(Status in ('Placed','Cancelled')),
+ReviewStatus nvarchar(20) check(Status in ('Passed','Failed','NotReviewed')),
+MemberShipCardID int not null,
+DedicatedServiceID int not null,
+foreign key(MemberShipCardID) references MemberShipCard(ID),
+foreign key(DedicatedServiceID) references DedicatedService(ID)
+
+);
+
+
+create table OrderTicket
+(
+ID int  not null auto_increment primary key,
+OrderedTime DateTime not null ,
+MemberShipCardID int not null,
+DedicatedServiceID int not null,
+foreign key(MemberShipCardID) references MemberShipCard(ID),
+foreign key(DedicatedServiceID) references DedicatedService(ID)
+
+);
+
+
+create table PaymentMethod
+(
+ID int not null primary key auto_increment,
+Name nvarchar(20) not null
+);
+
+
+
+create table PaymentRecord
+(
+ID int not null primary key auto_increment,
+Amount decimal(10,2) not null,
+TimeFinished DateTime not null,
+PaymentMethodID int not null,
+CashierID int not null,
+foreign key(PaymentMethodID) references PaymentMethod(ID),
+foreign key(CashierID) references Employee(ID)
+);
+
+
+
+
+
+
+drop database TSP
---
+++ SQL/TSP_TestData.sql
@@ -0,0 +1,16 @@
+use TSP;
+
+-- Init User Table
+
+insert into User values ('Von',1,'von@xxx.com','M',28,'2333','p@ssw0rd');
+insert into User values ('Gary',1,'gary@xxx.com','M',24,'2333','p@ssw0rd');
+insert into User values ('Ann',1,'ann@xxx.com','F',19,'2333','p@ssw0rd');
+insert into User values ('Peter',1,'peter@xxx.com','M',26,'2333','p@ssw0rd');
+
+-- Init MembershipCardLevel Table
+insert into MemberShipCardLevel values (0,'');
+insert into MemberShipCardLevel values (0,'test');
+insert into MemberShipCardLevel values (0,'test');
+insert into MemberShipCardLevel values (0,'test');
+
+
