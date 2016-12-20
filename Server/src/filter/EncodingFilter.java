260fddd626fe77960e622c53f9858ac2431d8a85
Verrickt <Adios_Verrickt@outlook.com>
2016-12-20 23:24:42 +0800

    Set content type for response in filter

--- Server/src/filter/EncodingFilter.java
+++ Server/src/filter/EncodingFilter.java
@@ -17,6 +17,7 @@ public class EncodingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+		response.setContentType("text/html; Charset=utf-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		chain.doFilter(request, response);
